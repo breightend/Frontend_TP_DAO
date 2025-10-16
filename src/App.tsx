76983c80eab297b4a56fc5aa314762901b6e5e19
Brenda Tapari <99502@sistemas.frc.utn.edu.ar>
2025-10-16 17:54:14 -0300

    comenzando el landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
+  
   return (
     <>
+      <div className="flex flex-col items-center justify-center bg-base-100 mt-4 gap-4">
+        <h1 className="text-2xl">Polymorph-Rides</h1>
+        <h2 className="text-xl"> el mismo auto, pero muchos propositos!</h2>
+      </div>
 
+      <div className="grid grid-cols-4 mt-4 gap-2">
+        <div className="card bg-base-200 shadow-xl bg-gradient-to-b from-blue-200 to-purple-200 m-4">
+          <div className="card-body">
+            <h3 className="card-title">Clientes, Vehiculos y Empleados </h3>
+            <p className="card-text">Consulta la informacion detallada de cada uno aqui</p>
+          </div>
+        </div>
+        <div className="card bg-base-200 shadow-xl bg-gradient-to-b from-blue-200 to-purple-200 m-4">
+          <div className="card-body">
+            <h3 className="card-title">Registro de alquileres</h3>
+            <p className="card-text">Informacion sobre los alquileres realizados.</p>
+          </div>
+        </div>
+        <div className="card bg-base-200 shadow-xl bg-gradient-to-b from-blue-200 to-purple-200 m-4">
+          <div className="card-body">
+            <h3 className="card-title">Control de mantenimiento</h3>
+            <p className="card-text">Consulta la informacion sobre el mantenimiento de los vehiculos aqui</p>
+          </div>
+        </div>
+        <div className="card bg-base-200 shadow-xl bg-gradient-to-b from-blue-200 to-purple-200 m-4">
+          <div className="card-body">
+            <h3 className="card-title">Reportes</h3>
+            <p className="card-text">Consulta la informacion sobre los reportes generados aqui</p>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
 export default function Navbar() {
     return(
         <>
-        
+            <div className="navbar bg-primary/50">
+                <div className="flex-1">
+                    <a className="btn btn-ghost normal-case text-xl"> Polymorph-Rides</a>
+                </div>
+            </div>
         </>
     )
 }
